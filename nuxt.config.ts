1d92a41be86889e41b086d58ba76b04dbf9c9c27
George Stojanovic <94017086+itimers@users.noreply.github.com>
2025-12-02 15:17:58 +0100

    next test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,40 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   nitro: {
+    prerender: {
+      routes: [
+        '/',
+      ],
+      crawlLinks: false,
+    },
+    routeRules: {
+      '/**': {
+        headers: {
+          'Content-Language': 'sr',
+        },
+      },
+      '/_nuxt/**': {
+        headers: {
+          'cache-control': 'public, max-age=31536000, immutable',
+          vary: 'Accept-Encoding',
+        },
+      },
+    },
+    publicAssets: [
+      {
+        dir: 'static/img',
+        maxAge: 60 * 60 * 24 * 365,
+      },
+      {
+        dir: 'public/fonts',
+        maxAge: 60 * 60 * 24 * 365,
+      },
+    ],
+    minify: true,
     preset: 'github-pages',
   },
   app: {
     baseURL: import.meta.env.PROD ? '/nuxt-cookie-system/' : '/',
-    buildAssetsDir: '/nuxt-cookie-system/_nuxt/',
-    cdnURL: import.meta.env.PROD ? '/nuxt-cookie-system/' : undefined,
     head: {
       script: [
         {
